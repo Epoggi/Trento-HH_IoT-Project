55358b38b9b7f3ba2db921dd2ab9497e70dd54ee
Epoggi <11327084+Epoggi@users.noreply.github.com>
2021-04-15 12:16:16 +0300

    update test.js
    getting structure ready for testing charts.js

--- coronafront/src/tests/test.js
+++ coronafront/src/tests/test.js
@@ -1,9 +1,54 @@
 var assert = require('assert');
+//get functions from Charts.js
+const {Charts} = require('../pages/Charts.js')
 
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('Charts.js function tests', function(){
+  describe('Check Risks functions', function(){
+    describe('Check all risks', function(){
+      it('testing..', function(){
+        
+    const testData = [
+      {
+          "name": "Location",
+          "time": 1614944496619605500,
+          "room": "lab",
+          "tagID": 1,
+          "x": 2.693613716183034,
+          "y": 3.5775570877129788
+      },
+      {
+        "name": "Location",
+        "time": 1614944496619605500,
+        "room": "lab",
+        "tagID": 2,
+        "x": 2.693613716183034,
+        "y": 3.5775570877129788
+    },
+
+  ]
+
+        let result = Charts.checkRisk(testData)
+
+        assert.deepStrictEqual(result, [
+          {
+            //expected risk results
+          }
+        ])
+      });
+    });
+    describe('Check room risks', function(){
+
+    });
+    describe('Check individual risks', function(){
+
     });
   });
 });
